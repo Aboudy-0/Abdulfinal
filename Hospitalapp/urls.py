6e2a225f6f5ce169ea37a1af5baf8145acbb1e3b
Aboudy-0 <aboudshiek17@gmail.com>
2025-02-28 16:20:50 +0300

    first commit

---
+++ Hospitalapp/urls.py
@@ -0,0 +1,16 @@
+
+from django.contrib import admin
+from django.urls import path
+from Hospitalapp import views
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', views.index, name='index'),
+    path('starter/', views.starter, name='starter'),
+    path('about/', views.about, name='about'),
+    path('service/', views.services, name='service'),
+    path('department/', views.department, name='department'),
+    path('doctors/', views.doctors, name='doctors'),
+    path('contacts/', views.contacts, name='contacts'),
+    path('appointment/', views.appointment, name='appointment'),
+]
---
+++ Hospitalapp/views.py
@@ -0,0 +1,48 @@
+from django.shortcuts import render, redirect
+from Hospitalapp.models import *
+# Create your views here.
+def index(request):
+    return render(request,'index.html')
+
+def starter(request):
+    return render(request,'starter-page.html')
+
+def about(request):
+    return render(request,'about.html')
+
+def services(request):
+    return render(request,'services.html')
+
+def department(request):
+    return render(request,'department.html')
+
+def doctors(request):
+    return render(request,'doctors.html')
+
+def contacts(request):
+    if request.method == "POST":
+        mycontact = Contact(
+            name = request.POST['name'],
+            email = request.POST['email'],
+            subject = request.POST['subject'],
+            message = request.POST['message'],
+        )
+        mycontact.save()
+        return redirect('/contacts')
+    else:
+        return render(request, 'contacts.html')
+
+def appointment(request):
+    if request.method == "POST":
+        myappointment = Appointment(
+            name=request.POST['name'],
+            email=request.POST['email'],
+            phone=request.POST['phone'],
+            date=request.POST['date'],
+            department=request.POST['department'],
+            doctor=request.POST['doctor'],
+            message=request.POST['message'])
+        myappointment.save()
+        return redirect('/appointment')
+    else:
+        return render(request, 'appointment.html')
